3d711e0e3456da3b561dcd68a528cb7d802e72db
Debora Carvalho <dbndy3@gmail.com>
2023-11-17 20:57:05 -0300

    Fixing the bugs
    The bug of camera2d and the animated sprites from main.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,43 +1,42 @@
 extends KinematicBody2D
 
 #Variaveis
-var speed = 50
-var motion = Vector2.ZERO
+export var moveSpeed = 100
+var collision = null
 
 #Movimento de controle do player
 
 func _physics_process(delta):
+	var moveVector = Vector2()
+	
 	if Input.is_action_pressed("right"):
-		motion.x = speed
+		moveVector.x += 1
 
 		
 		$AnimatedSprite.play("sidewalk-dir")
-		$AnimatedSprite.flip_h = false
 		
 	elif Input.is_action_pressed("left"):
-		motion.x = -speed
+		moveVector.x -= 1
 
 		
 		$AnimatedSprite.play("sidewalk-esq")
 		
 	elif Input.is_action_pressed("down"):
-		motion.y = speed
-
+		moveVector.y += 1
 		
 		$AnimatedSprite.play("down_walk")
 		
 	elif Input.is_action_pressed("up"):
-		motion.y = -speed
-
+		moveVector.y -= 1
 		
 		$AnimatedSprite.play("up_walk")
-		
+	
 	else:
 		$AnimatedSprite.play("default")
 		
-		motion.x = 0
-		motion.y = 0
-	
-	move_and_slide(motion)
+		moveVector.x = 0
+		moveVector.y = 0
+	collision = move_and_collide(moveVector.normalized() * delta * moveSpeed)
 	
 # move_and_slide é uma função construtora da Godot que aplica o movimento ao personagem mas também o processo físico que checa as colisões. (2 funções combinadas)
+#Diferente do anterior move_and_slide, o move_and_collide deixa o personagem estatico ao colidir.
