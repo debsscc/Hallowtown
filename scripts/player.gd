ce8c7ded3a8543eada204e8273fc84245b0d393f
Debora Carvalho <dbndy3@gmail.com>
2023-11-17 21:50:21 -0300

    Reuniao com o bernardo

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 #Variaveis
 export var moveSpeed = 100
 var collision = null
+var running = 1
 
 #Movimento de controle do player
 
@@ -11,32 +12,49 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		moveVector.x += 1
-
 		
-		$AnimatedSprite.play("sidewalk-dir")
-		
-	elif Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left"):
 		moveVector.x -= 1
-
 		
-		$AnimatedSprite.play("sidewalk-esq")
-		
-	elif Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down"):
 		moveVector.y += 1
 		
-		$AnimatedSprite.play("down_walk")
-		
-	elif Input.is_action_pressed("up"):
+	if Input.is_action_pressed("up"):
 		moveVector.y -= 1
-		
-		$AnimatedSprite.play("up_walk")
 	
+	if moveVector.x == 0:
+		if moveVector.y < 0:
+			$AnimatedSprite.play("up_walk")
+		elif moveVector.y > 0:
+			$AnimatedSprite.play("down_walk")
 	else:
-		$AnimatedSprite.play("default")
-		
-		moveVector.x = 0
-		moveVector.y = 0
-	collision = move_and_collide(moveVector.normalized() * delta * moveSpeed)
+		if moveVector.x > 0:
+			$AnimatedSprite.play("sidewalk-dir")
+		elif moveVector.x < 0:
+			$AnimatedSprite.play("sidewalk-esq")
 	
+	if (moveVector.x == 0 and moveVector.y == 0):
+		$AnimatedSprite.play("default")
+
 # move_and_slide é uma função construtora da Godot que aplica o movimento ao personagem mas também o processo físico que checa as colisões. (2 funções combinadas)
 #Diferente do anterior move_and_slide, o move_and_collide deixa o personagem estatico ao colidir.
+
+	#Verifica se a tecla de corrida está sendo pressionada
+	if Input.is_action_pressed("run"):
+		running = 2
+		print("Run key pressed")
+
+	else:
+		running = 1
+		
+	#Atualiza a velocidade de acordo com a condição de corrida
+	if running: 
+		moveVector *= 2 #Dobra a velocidade se a corrida estiver ativa
+		
+		print("Running:", running, "Velocity doubled")
+  # Mensagem de depuração
+	collision = move_and_collide(moveVector.normalized() * delta * moveSpeed * running)
+
+
+
+
