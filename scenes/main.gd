115c2b094bcacc2350a9b435549ac2468181e3bd
Debora Carvalho <dbndy3@gmail.com>
2023-12-13 00:53:58 -0300

    Aumento da área do ciclo dia/noite, alteração icon do jogo, codigos da tela de pause e configuração do menu animado
    :) ! ta lindo

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+onready var backgroundSound = $som_background
 onready var audio_donaMorte = $Dona_MorteSFX
 var donaMorteSoundPlayed = false  # Variável para controlar se o som já foi reproduzido
 
+var pause = pause
+
+func _ready():
+	pause = $UserInterface/pause
+	pause.hide()
 #func _on_Dona_Morte_body_entered(body):
 #	print("Body entered")
 #	if body == $Player and not donaMorteSoundPlayed:  
@@ -14,4 +20,19 @@ var donaMorteSoundPlayed = false  # Variável para controlar se o som já foi re
 #			donaMorteSoundPlayed = true
 #func _on_Dona_MorteSFX_finished():
 #	audio_donaMorte.stop()
+func _process(delta):
+	# Verifica se o botão "esq" está pressionado
+	if Input.is_action_pressed("esq"):
+		# Se estiver pressionado e a sprite estiver escondida, mostre-a
+		if !pause.visible:
+			pause.show()
+		if pause.visible:
+			backgroundSound.stop()
 
+func _on_Exit_Game_pressed():
+	get_tree().change_scene("res://scenes/menu.tscn")
+
+
+func _on_Resume_pressed():
+	pause.hide()
+	backgroundSound.play()
