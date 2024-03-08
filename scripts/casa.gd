8c5ad0680d0a556aa0158cacc25bf2c8184a757f
Debora Carvalho <dbndy3@gmail.com>
2024-03-08 17:09:05 -0300

    ADD: torneira e escada dialogo

--- scripts/casa.gd
+++ scripts/casa.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
 
-
 func _on_exit_casinha_body_entered(body):
 		if body.has_method("player_exit_method"):
 			get_tree().change_scene("res://scenes/farm.tscn")
+
+func _on_torneira_body_entered(body):
+	if body.has_method("player_exit_method"):
+		var dialogo_torneira = Dialogic.start('torneira')
+		add_child(dialogo_torneira)
+	
+
+func _on_escadas_body_entered(body):
+	if body.has_method("player_exit_method"):
+		var dialogo_escadas = Dialogic.start('escadas')
+		add_child(dialogo_escadas)
+
+
+func _on_cama_body_entered(body):
+	if body.has_method("player_exit_method"):
+		var dialogo_cama = Dialogic.start('cama')
+		add_child(dialogo_cama)
