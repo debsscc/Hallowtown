3005dd859cb97a15b792cb2c6267c4e323f926ed
Debora Carvalho <dbndy3@gmail.com>
2024-03-13 15:34:57 -0300

    ADD: Tutorial

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,11 +2,13 @@ extends Control
 
 onready var video_introducao = $"Introducao-Abobora"
 onready var audio_introducao = $AudioStreamPlayer
+onready var tutorial = $tutorial
 
 
 func _ready():
 	video_introducao.hide()
 	video_introducao.stop()
+	tutorial.stop()
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("enter"):
@@ -17,7 +19,9 @@ func _on_Button_pressed():
 	video_introducao.show()
 	video_introducao.play()
 	$Button.hide()
-	
 
 func _on_IntroducaoAbobora_finished():
+	tutorial.play()
+
+func _on_tutorial_finished():
 	get_tree().change_scene("res://scenes/main.tscn")
